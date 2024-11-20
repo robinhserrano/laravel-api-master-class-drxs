35a0677f4b151b1da1ba061711be1ee46f529be6
robinhserrano <88170303+robinhserrano@users.noreply.github.com>
2024-11-20 12:01:53 +0800

    03 How to Version Your API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,11 @@
 
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Models\Ticket;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/tickets', function() {
+    return Ticket::all();
+});
