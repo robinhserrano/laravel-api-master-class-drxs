007f4b962e27f83dbb0c1f28b8ffbbd01e9dc324
robinhserrano <88170303+robinhserrano@users.noreply.github.com>
2024-11-20 11:40:42 +0800

    02 - Working with Postman

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,5 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
