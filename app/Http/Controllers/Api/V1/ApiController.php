d023d7133ba822ecb02348d2e093ea48385b9783
robinhserrano <88170303+robinhserrano@users.noreply.github.com>
2024-11-22 11:50:06 +0800

    13 - Creating Resources with Post Requests

--- app/Http/Controllers/Api/V1/ApiController.php
+++ app/Http/Controllers/Api/V1/ApiController.php
@@ -1,9 +1,10 @@
 <?php
 namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Traits\ApiResponses;
 class ApiController extends Controller
 {
+    use ApiResponses;
     public function include(string $relationship) : bool {
         $param = request()->get('include');
         if (!isset($param)) {
--- app/Http/Controllers/Api/V1/AuthorTicketsController.php
+++ app/Http/Controllers/Api/V1/AuthorTicketsController.php
@@ -1,15 +1,33 @@
 <?php
+
 namespace App\Http\Controllers\Api\V1;
+
 use App\Http\Controllers\Controller;
 use App\Http\Filters\V1\TicketFilter;
+use App\Http\Requests\Api\V1\StoreTicketRequest;
 use App\Http\Resources\V1\TicketResource;
 use App\Models\Ticket;
-use Illuminate\Http\Request;
+
 class AuthorTicketsController extends Controller
 {
-    public function index($author_id, TicketFilter $filters) {
+    public function index($author_id, TicketFilter $filters)
+    {
         return TicketResource::collection(
             Ticket::where('user_id', $author_id)->filter($filters)->paginate()
         );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store($author_id, StoreTicketRequest $request)
+    {
+        $model = [
+            'title' => $request->input('data.attributes.title'),
+            'description' => $request->input('data.attributes.description'),
+            'status' => $request->input('data.attributes.status'),
+            'user_id' => $author_id
+        ];
+        return new TicketResource(Ticket::create($model));
     }
 }
