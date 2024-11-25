b52b658e0d98f54259455fb7f72aca38263692a1
robinhserrano <88170303+robinhserrano@users.noreply.github.com>
2024-11-25 13:31:48 +0800

    17 - sing Policies for User Authorization

--- app/Http/Controllers/Api/V1/ApiController.php
+++ app/Http/Controllers/Api/V1/ApiController.php
@@ -1,16 +1,30 @@
 <?php
+
 namespace App\Http\Controllers\Api\V1;
+
 use App\Http\Controllers\Controller;
 use App\Traits\ApiResponses;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
 class ApiController extends Controller
 {
     use ApiResponses;
-    public function include(string $relationship) : bool {
+    use AuthorizesRequests;
+
+    protected $policyClass;
+
+    public function include(string $relationship): bool
+    {
         $param = request()->get('include');
         if (!isset($param)) {
             return false;
         }
         $includeValues = explode(',', strtolower($param));
         return in_array(strtolower($relationship), $includeValues);
+    }
+
+    public function isAble($ability, $targetModel)
+    {
+        return $this->authorize($ability, [$targetModel, $this->policyClass]);
     }
 }
