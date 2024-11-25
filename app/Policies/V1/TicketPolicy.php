5acd5efaef5025bd5edb84d42ee0736e6f9ce06e
robinhserrano <88170303+robinhserrano@users.noreply.github.com>
2024-11-25 14:09:47 +0800

    18 - Controlling Access with Token Abilities

--- app/Policies/V1/TicketPolicy.php
+++ app/Policies/V1/TicketPolicy.php
@@ -1,7 +1,11 @@
 <?php
+
 namespace App\Policies\V1;
+
 use App\Models\Ticket;
 use App\Models\User;
+use App\Permissions\V1\Abilities;
+
 class TicketPolicy
 {
     /**
@@ -11,8 +15,44 @@ class TicketPolicy
     {
         //
     }
-    public function update(User $user, Ticket $ticket) {
-        // TODO check for token ability
-        return $user->id === $ticket->user_id;
+
+    public function delete(User $user, Ticket $ticket)
+    {
+        if ($user->tokenCan(Abilities::DeleteTicket)) {
+            return true;
+        } else if ($user->tokenCan(Abilities::DeleteOwnTicket)) {
+            return $user->id === $ticket->user_id;
+        }
+
+        return false;
     }
- }
+
+    public function replace(User $user, Ticket $ticket)
+    {
+        if ($user->tokenCan(Abilities::ReplaceTicket)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function store(User $user, Ticket $ticket)
+    {
+        if ($user->tokenCan(Abilities::CreateTicket)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function update(User $user, Ticket $ticket)
+    {
+        if ($user->tokenCan(Abilities::UpdateTicket)) {
+            return true;
+        } else if ($user->tokenCan(Abilities::UpdateOwnTicket)) {
+            return $user->id === $ticket->user_id;
+        }
+
+        return false;
+    }
+}
